6d0c237b798cc6a3c7cde7e6a98e9b9e8fb4aa13
moolasreddy <moolasreddy@yahoo.com>
2025-03-11 07:44:58 -0400

    Update

---
+++ educationalalbum/diya/ImageGallery/app_list.js
@@ -0,0 +1,27 @@
+//imageContainer
+const imageContainer = document.querySelector(".image-container");
+
+//prevBtn
+const prevBtn = document.getElementById("prev");
+
+//homeBtn
+//const homeBtn = document.getElementById("home");
+
+//nextBtn
+const nextBtn = document.getElementById("next");
+
+let x = 0;
+
+prevBtn.addEventListener("click", () => {
+  x = x + 45;
+  rotate();
+});
+
+nextBtn.addEventListener("click", () => {
+  x = x - 45;
+  rotate();
+});
+
+function rotate() {
+  imageContainer.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+}
